ec12af094daad079ad29d70e7a3c8e8b72d1c69f
HauntedHalo <22328677+HauntedHalo@users.noreply.github.com>
2022-01-18 23:19:07 -0600

    Create helloWorld.c
    Testing Issues tab

---
+++ helloWorld.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+//  header file to input and output data.
+
+int main(){
+//  Main function for c, programs will always start from main.
+
+    pintf("Hello World!\n");
+//  printf function prints a string to the console. In this case, will print "Hello World" and then inserts a newline with "\n".
+
+//  Speaking of comments, this is a single line comment
+
+/*
+    This is a multi-line comment
+*/
+
+    return 0;
+//  returns 0, signaling to the program that it has terminated successfully.
+}
