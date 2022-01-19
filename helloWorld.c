4c05afb86ba65f4fba309af578bde8985261602e
dannydiep13 <71152429+dannydiep13@users.noreply.github.com>
2022-01-18 23:24:01 -0600

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -5,7 +5,7 @@
 int main(){
 //  Main function for c, programs will always start from main.
 
-    pintf("Hello World!\n");
+    printf("Hello World!\n");
 //  printf function prints a string to the console. In this case, will print "Hello World" and then inserts a newline with "\n".
 
 //  Speaking of comments, this is a single line comment
